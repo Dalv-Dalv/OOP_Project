c0279275b89817ea7287add7f3cf3653d13b397a
Dalv <futurevlad2@gmail.com>
2025-03-10 22:21:06 +0200

    Integrated Raylib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <raylib.h>
 
 int main() {
-	std::cout << "Hello";
+	InitWindow(600, 300, "Proiect POO");
+	SetTargetFPS(144);
+	SetConfigFlags(FLAG_VSYNC_HINT);
+
+	while(!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(BLACK);
+
+		DrawText("Pog", 20, 20, 30, ORANGE);
+
+		EndDrawing();
+	}
+	CloseWindow();
 	return 0;
 }
